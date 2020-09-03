0f3154db4b2c2f7251d7a802f1e982aa1c4dcc9e
Yaroslav Klochko <yaroklochko@gmail.com>
2020-09-04 01:31:43 +0800

    initial commit

---
+++ src/main/java/com/english/learnist/LearnistApplication.java
@@ -0,0 +1,13 @@
+package com.english.learnist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnistApplication.class, args);
+	}
+
+}
