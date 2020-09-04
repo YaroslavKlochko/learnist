4c9cb65be5bb70b31364ee8034a50ee176c41010
Yaroslav Klochko <yaroklochko@gmail.com>
2020-09-05 02:58:16 +0800

    #2 bootstrap successfully connected to the project

--- src/main/java/com/english/learnist/LearnistApplication.java
+++ src/main/java/com/english/learnist/LearnistApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnistApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LearnistApplication.class, args);
+    }
 
 }
--- src/main/java/com/english/learnist/controller/MainController.java
+++ src/main/java/com/english/learnist/controller/MainController.java
@@ -1,15 +1,14 @@
 package com.english.learnist.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/main")
+@RequestMapping(value = "/")
 public class MainController {
 
-    @GetMapping(value = {"/index", "/"})
+    @GetMapping(value = "/index")
     public String getMainPage() {
         return "index";
     }
