d865a884dd1385757d48245ab2edfce8eed30080
Yaroslav <43871314+IamAxWeLL@users.noreply.github.com>
2020-10-11 06:43:02 +0800

    #8 fixed failed merge (#22)
    * #8 fixed failed merge
    
    * #8 fixed issues with merge 2.0

--- src/main/java/com/learnist/config/WebSecurityConfig.java
+++ src/main/java/com/learnist/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
-package com.english.learnist.config;
+package com.learnist.config;
 
+import com.learnist.database.repository.UserRepository;
+import com.learnist.database.service.AdminDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,8 +15,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static java.lang.Boolean.TRUE;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserRepository userRepository;
+
+    public WebSecurityConfig(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,17 +48,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login");
     }
 
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .and()
-                .withUser("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN");
+        auth.userDetailsService(getUserDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean(name = "userDetailsService")
+    public AdminDetailsService getUserDetailsService() {
+        return new AdminDetailsService(userRepository);
     }
 
     @Bean
--- src/main/java/com/learnist/domain/User.java
+++ src/main/java/com/learnist/domain/User.java
@@ -7,17 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
