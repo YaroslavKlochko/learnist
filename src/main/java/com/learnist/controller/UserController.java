1cfe592b10629416793acfc371624a5a7b2d7c43
Yaroslav <43871314+IamAxWeLL@users.noreply.github.com>
2020-10-22 20:32:45 +0800

    Lr 011 be change format logging (#25)
    * #9 BE for user was added
    
    * #9 BE user add functionality was implemented and bug related to error page 500 was fixed
    
    * #10 added FE form for add user
    
    * #10 code refactoring(unused dependencies were removed)
    
    * #10 code clean up
    
    * #11 logback.xml was added
    
    * #11 file appender was configured

--- src/main/java/com/learnist/controller/UserController.java
+++ src/main/java/com/learnist/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 @RequestMapping(value = "user")
 public class UserController {
--- src/main/java/com/learnist/database/service/UserService.java
+++ src/main/java/com/learnist/database/service/UserService.java
@@ -3,9 +3,15 @@ package com.learnist.database.service;
 import com.learnist.database.repository.UserRepository;
 import com.learnist.domain.User;
 import com.learnist.domain.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
 
@@ -14,10 +20,12 @@ public class UserService {
     }
 
     public User addUser(final UserDTO user) {
+        logger.info("This is info logging level");
         User newUser = new User();
         newUser.setUsername(user.getUsername());
         newUser.setPassword(user.getPassword());
         userRepository.save(newUser);
+        logger.trace("Trace logging level in  " + UserService.class.getSimpleName());
         return newUser;
     }
 }
