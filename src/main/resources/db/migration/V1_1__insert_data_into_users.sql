c8ff4975da3260ba0cff6c5810917cd73e4e6d63
Yaro Klochko <yaroklochko@gmail.com>
2020-12-06 08:39:14 +0800

    #20 add a few more buttons to the header and avatar icon

--- src/main/resources/db/migration/V1_1__insert_data_into_users.sql
+++ src/main/resources/db/migration/V1_1__insert_data_into_users.sql
@@ -1,12 +1,9 @@
-INSERT INTO users (username, password, email, enabled, account_expired, credentials_expired, account_non_locked)
-VALUES ('admin_Yaro', '$2a$10$D9NsSU.I28ze6bdmpeK1ROt1p5Nij1NgRbip4xjmCjEfyT1h9J0OS', 'axwell33@gmail.com',
-        TRUE, TRUE, TRUE, TRUE);
+INSERT INTO users (username, password, email)
+VALUES ('admin_Yaro', '$2a$10$D9NsSU.I28ze6bdmpeK1ROt1p5Nij1NgRbip4xjmCjEfyT1h9J0OS', 'axwell33@gmail.com');
 
-INSERT INTO users (username, password, email, enabled, account_expired, credentials_expired, account_non_locked)
-VALUES ('user_Bob', '$2a$10$7eVXRzFSz385WpByla5pO.L5a5OSa0oZ8IlgB2Lxj5aaNlKqu7r0O', 'bob@gmail.com',
-        TRUE, TRUE, TRUE, TRUE);
+INSERT INTO users (username, password, email)
+VALUES ('user_Bob', '$2a$10$7eVXRzFSz385WpByla5pO.L5a5OSa0oZ8IlgB2Lxj5aaNlKqu7r0O', 'bob@gmail.com');
 
-INSERT INTO users (username, password, email, enabled, account_expired, credentials_expired, account_non_locked)
+INSERT INTO users (username, password, email)
 VALUES ('creator_Anastasiia', '$2a$10$D9NsSU.I28ze6bdmpeK1ROt1p5Nij1NgRbip4xjmCjEfyT1h9J0OS',
-        'anastasiiaforwork@gmail.com',
-        TRUE, TRUE, TRUE, TRUE);
+        'anastasiiaforwork@gmail.com');
